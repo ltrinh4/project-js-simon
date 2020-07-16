9e68595fd64291c2e49f14d6fd7f9fbad11aec23
mndlovu18 <mndlovu@brynmawr.edu>
2020-07-16 15:17:42 -0400

    _thirdCommit!!

--- site/buttons.js
+++ site/buttons.js
@@ -2,66 +2,71 @@ var clicks = 0;
 
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('click', function() {
-  var myMusic = document.getElementById('music');
-  myMusic.play();
-clicks += 1;
-document.getElementById('clicks').innerHTML = clicks;
-if (clicks === 1){
- document.getElementById('play').innerHTML = 'Click Blue';
-} else {
- alert("You Suck!");
- location.reload();
-}
+ _playMusic();
+clickIncrement();
+randomGenerator();
 });
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
-  var myMusic = document.getElementById('music');
-  myMusic.play();
-  clicks += 1;
-  document.getElementById('clicks').innerHTML = clicks;
-   if (clicks === 3){
- document.getElementById('play').innerHTML = 'Click Yellow';
-} else {
- alert("You Suck!");
- location.reload();
-}
-
+  _playMusic();
+ clickIncrement();
+  randomGenerator();
 });
 
 let yellowButton = document.querySelector('.simon-button.yellow');
 yellowButton.addEventListener('click', function() {
-  var myMusic = document.getElementById('music');
-  myMusic.play();
-  clicks += 1;
-  document.getElementById('clicks').innerHTML = clicks;
-  if (clicks === 4){
- document.getElementById('play').innerHTML = 'The End';
- alert('The End!');
- location.reload();
-} else {
- alert("You Suck!");
-location.reload();
-}
+  _playMusic();
+ clickIncrement();
+  randomGenerator();
 
 });
 
 let blueButton = document.querySelector('.simon-button.blue');
 blueButton.addEventListener('click', function() {
-  var myMusic = document.getElementById('music');
-  myMusic.play();
-  clicks += 1;
-  document.getElementById('clicks').innerHTML = clicks;
-  if (clicks === 2){
- document.getElementById('play').innerHTML = 'Click Red';
-} else {
- alert("You Suck!");
- location.reload();
-}
+  _playMusic();
+  clickIncrement();
+  randomGenerator();
 });
 
 let playButton = document.querySelector('#play');
 playButton.addEventListener('click', function() {
-    document.getElementById('play').innerHTML = 'Click Green';
+randomGenerator();
 
 });
+
+
+function _playMusic(){
+  var myMusic = document.getElementById('music');
+  myMusic.play();
+}
+
+function clickIncrement(){
+ clicks += 1;
+ document.getElementById('clicks').innerHTML = clicks;
+}
+function randomGenerator(){
+	var randomNumber = Math.floor(Math.random() * 4) + 1;
+	console.log(randomNumber);
+	
+	switch(randomNumber){
+		case 1:
+		document.getElementById('play').innerHTML = 'Click Red';
+		break;
+		
+		case 2:
+		document.getElementById('play').innerHTML = 'Click 	Blue';
+		break;
+		
+		case 3:
+		document.getElementById('play').innerHTML = 'Click Yellow';
+		break;
+		
+		case 4:
+		document.getElementById('play').innerHTML = 'Click Green';
+		break;
+			
+	}
+}
+
+
